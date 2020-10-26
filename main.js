f18406e785178ce6f784b8fa01272d0016ed05e3
Manuel <manuel.devito@icloud.com>
2020-10-26 17:25:58 +0100

    Versione 2 esercizio

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+
+
 $('.fa-bars').click(function(){
     $('.hamburger-menu').show();
 });
 
 $('.fa-times').click(function(){
     $('.hamburger-menu').hide();
+})
+
+
+// Versione 2
+
+$('.fa-bars').click(function(){
+    $('.hamburger-menu').fadeIn('fast');
+});
+
+$('.fa-times').click(function(){
+    $('.hamburger-menu').fadeOut('slow');
 })
